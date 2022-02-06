50fe8599415314838a7f5e148e2ae2c9e40c1855
MorganDbs <mailmorgandubois@gmail.com>
2022-02-06 17:24:25 +0100

    fixing refresh bug

--- src/Statefull/newListForm.tsx
+++ src/Statefull/newListForm.tsx
@@ -1,33 +1,68 @@
-import React, {ChangeEvent, Component, FormEvent, useState} from 'react';
-import List from '../Stateless/List';
+import React, { ChangeEvent, Component, FormEvent, useState } from "react";
+import List from "../Stateless/List";
+import { v4 as uuid } from "uuid";
 
+class NewListForm extends Component<NewListFormType, { listTitle: string }> {
+  state = {
+    listTitle: "",
+  };
 
-class NewListForm extends Component <NewListFormType> {
+  handleTitleChange = (e: any) => {
+    this.setState({
+      ...this.state,
+      listTitle: e.target.value,
+    });
+  };
 
-  
+  handleClick = (e: any) => {
+    this.props.onCreate(uuid(), uuid(), this.state.listTitle);
+    this.setState({
+      listTitle: "",
+    });
+    e.preventDefault();
+  };
 
   //Pour l'instant cela ne change que le titre, voir comment ajouter une bloc "list" complet (instancier la class ? new List() ?)
   render() {
-    const {onCreate, filterText, onhandleFilterTextChange} = this.props;
+    const { onCreate, filterText, onhandleFilterTextChange } = this.props;
+
     return (
-      <>
-        <div className="row">
-          <form className="form-group">
-            <div className="col-4 p-1">
-                <label htmlFor="new_title" style={{color: 'white'}}>Créez une nouvelle liste :</label>
-                <input type="text" id="new_title" className="form-control"/>
-                <button type="submit" className='btn btn-primary' onClick={() => {onCreate()}}>Créer</button>
-                {/* <input type="hidden" id="new_list" />  */}
-            </div>
-            <div className="col-4 p-1">
-            <label htmlFor="filter" style={{color: 'white'}}>filter :</label>
-            <input type="text" id="filter" className="form-control" value={filterText} onChange={() => {onhandleFilterTextChange()} }/>
-            </div>
-          </form>
-        </div>
-      </>
+      <div className="row">
+        <form className="form-group">
+          <div className="col-4 p-1">
+            <label htmlFor="new_title" style={{ color: "white" }}>
+              Créez une nouvelle liste :
+            </label>
+            <input
+              type="text"
+              id="new_title"
+              className="form-control"
+              value={this.state.listTitle}
+              onChange={this.handleTitleChange}
+            />
+            <button className="btn btn-primary" onClick={this.handleClick}>
+              Créer
+            </button>
+            {/* <input type="hidden" id="new_list" />  */}
+          </div>
+          <div className="col-4 p-1">
+            <label htmlFor="filter" style={{ color: "white" }}>
+              filter :
+            </label>
+            <input
+              type="text"
+              id="filter"
+              className="form-control"
+              value={filterText}
+              onChange={() => {
+                onhandleFilterTextChange();
+              }}
+            />
+          </div>
+        </form>
+      </div>
     );
-  };
+  }
 }
 
 export default NewListForm;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,88 +1,96 @@
-
 /**
  * Component Types
  */
 type ContainerType = {
-  children: Component
-}
+  children: Component;
+};
 
 type NewListFormType = {
-  filterText :string,
-  onCreate :Function,
-  onhandleFilterTextChange :Function
-}
+  filterText: string;
+  onCreate: Function;
+  onhandleFilterTextChange: Function;
+};
 
 type ListType = {
-  key    :number,
-  id     :number,
-  title  :string,
-}
+  key: number;
+  id: number;
+  title: string;
+};
 
 type Task = {
-  id          :number,
-  text        :string,
-  description :string,
-  assignedTo  :string,
-  priority    :string,
-  completed   :boolean,
-}
+  id: number;
+  text: string;
+  description: string;
+  assignedTo: string;
+  priority: string;
+  completed: boolean;
+};
 
 /**
  * Props Types
  */
 
 type ListPropsType = {
-  id      :number,
-  title   :string,
-  delete  :(id :number) => void
-}
+  id: number;
+  title: string;
+  delete: (id: number) => void;
+};
 
 type TaskPropsType = {
-  id          :number,
-  text        :string,
-  description :string,
-  assignedTo  :string,
-  priority    :string,
-  completed   :boolean,
-  toggleComplete  :(id :number) => void
-  delete          :(id :number) => void
-  update          :(id :number, text :string, description :string, assignedTo :string, priority :string) => void
-}
+  id: number;
+  text: string;
+  description: string;
+  assignedTo: string;
+  priority: string;
+  completed: boolean;
+  toggleComplete: (id: number) => void;
+  delete: (id: number) => void;
+  update: (
+    id: number,
+    text: string,
+    description: string,
+    assignedTo: string,
+    priority: string
+  ) => void;
+};
 
-type confirmUpdate = (id :number, titleUpdated :string, descriptionUpdated :string, assignedToUpdated :string, priorityUpdated :string) => void;
+type confirmUpdate = (
+  id: number,
+  titleUpdated: string,
+  descriptionUpdated: string,
+  assignedToUpdated: string,
+  priorityUpdated: string
+) => void;
 
 type handleConfirmUpdate = (e: MouseEvent) => void;
 
 type ButtonTypeProps = {
-  type     :submit,
-  color    :string,
-  children :string,
-  clic     :React.MouseEventHandler<HTMLButtonElement>
-}
+  type: submit;
+  color: string;
+  children: string;
+  clic: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 type NonControlledInputProps = {
-  name     :string,
-  children :string,
-  
-}
+  name: string;
+  children: string;
+};
 
 type ControlledInputProps = {
-  name     :string, 
-  value    :string, 
-  change   :Function, 
-  children :string,
-  onChange :ChangeEventHandler
-}
-
-
+  name: string;
+  value: string;
+  change: Function;
+  children: string;
+  onChange: ChangeEventHandler;
+};
 
 /**
  * Functions Types
  */
-type HandeDeleteTask  = (e :MouseEvent) => void;
-type HandeDeleteList  = (e :MouseEvent) => void;
+type HandeDeleteTask = (e: MouseEvent) => void;
+type HandeDeleteList = (e: MouseEvent) => void;
 
-type ToggleComplete   = (id :number) => void;
+type ToggleComplete = (id: number) => void;
 
 // type AddList     = (newList :List) => void;
 // type CreateTask  = (newTask :string) => void;
